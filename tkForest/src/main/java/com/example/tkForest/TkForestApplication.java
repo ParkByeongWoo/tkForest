02ccb6a554e55042b8e3ad0fd39d47420673948e
ParkByeongWoo <261pbw@gmail.com>
2024-10-07 12:23:11 +0900

    no message

---
+++ tkForest/src/main/java/com/example/tkForest/TkForestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tkForest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkForestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkForestApplication.class, args);
+	}
+
+}
---
+++ tkForest/src/test/java/com/example/tkForest/TkForestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tkForest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkForestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
