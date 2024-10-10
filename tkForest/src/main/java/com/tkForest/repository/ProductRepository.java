3fe6efe109bfbd44c013fbcd51d4b686b843b3d0
JI <jiyoo3104@gmail.com>
2024-10-10 10:52:10 +0900

    1. application 서버 구글클라우드로 변경 2. product관련 파일들 생성

---
+++ tkForest/src/main/java/com/tkForest/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tkForest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tkForest.entity.ProductEntity;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+}
