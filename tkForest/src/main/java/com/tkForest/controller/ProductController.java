afb74ce8e44e760b479f2a5ad7573b09eccd589d
JI <jiyoo3104@gmail.com>
2024-10-10 12:28:17 +0900

    Product부분 재시도

---
+++ tkForest/src/main/java/com/tkForest/controller/ProductController.java
@@ -0,0 +1,47 @@
+package com.tkForest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tkForest.dto.ProductDTO;
+import com.tkForest.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+// 컨트롤러	
+	
+	final ProductService productService;
+	
+	/**
+	 * 상품 등록 화면을 요청
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/productCreate")
+	public String productCreate(Model model) {
+		
+		log.info("상품 등록 화면 요청 받음");
+		return "product/productCreate";
+	}
+	
+	
+	public String productOne() {
+		Long productNum = 1L;
+		
+		ProductDTO product = productService.selectOne(productNum);
+		log.info("조회된 상품: {}", product.toString());
+		
+		return null;
+	}
+	
+
+}
