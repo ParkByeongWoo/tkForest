c40b49812810a3930899338ca34166eeaa582759
JI <jiyoo3104@gmail.com>
2024-10-15 18:10:40 +0900

    1015 작업(nation 엔티티 DTO 생성까지)

--- tkForest/src/main/java/com/tkForest/controller/UserController.java
+++ tkForest/src/main/java/com/tkForest/controller/UserController.java
@@ -1,19 +1,14 @@
 package com.tkForest.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tkForest.dto.BuyerDTO;
 import com.tkForest.dto.SellerDTO;
 import com.tkForest.service.UserService;
-
-import jakarta.validation.Valid;
 
 //import com.tkForest.service.ProductService;
 
@@ -53,7 +48,7 @@ public class UserController {
     * 회원가입(셀러) 처리
     * 전달받은 sellerDTO를 sellerEntity로 변경한 후에 DB에 저장
     * @param sellerDTO
-	 * @return boolean
+	* @return boolean
     */
    @PostMapping("/sellerSignUp")
    public String sellerSignUp(@ModelAttribute SellerDTO sellerDTO) {
@@ -66,6 +61,7 @@ public class UserController {
    	// UserService를 통해 셀러 회원가입 처리 로직 호출
    	boolean result = userService.sellerSignUp(sellerDTO);
    	log.info("셀러 회원가입 성공여부: {}", result);
+   	log.info("셀러 회원가입 정보: {}", sellerDTO);
 
    	return "redirect:/";  // 회원가입 완료 후 로그인 페이지로 리다이렉트
 //   	return "redirect:/user/login";  // 회원가입 완료 후 로그인 페이지로 리다이렉트
@@ -82,21 +78,25 @@ public class UserController {
 
    /**
     * 회원가입(바이어) 처리
-    * 전달받은 buyerDTO를 buyerEntity로 변경한 후에 DB에 저장
+    * 전달받은 buyerDTO를 BuyerEntity로 변경한 후에 DB에 저장
     * @param buyerDTO
-	 * @return boolean
+	* @return boolean
     */
    @PostMapping("/buyerSignUp")
-   public String buyerSignUp(
-   		@ModelAttribute BuyerDTO buyerDTO
-   		) {
-   	 log.info("BuyerDTO: {}", buyerDTO.toString());
+   public String buyerSignUp(@ModelAttribute BuyerDTO buyerDTO) {
    	
-   	// UserService를 통해 셀러 회원가입 처리 로직 호출
+	log.info("BuyerDTO: {}", buyerDTO.toString());
+   	
+	// 기본값으로 설정
+	buyerDTO.setBuyerStatus(true);
+	
+   	// UserService를 통해 바이어 회원가입 처리 로직 호출
    	boolean result = userService.buyerSignUp(buyerDTO);
    	log.info("바이어 회원가입 성공여부: {}", result);
+   	log.info("바이어 회원가입 정보: {}", buyerDTO);
 
-   	return "redirect:/user/login";  // 회원가입 완료 후 로그인 페이지로 리다이렉트
+   	return "redirect:/";  // 회원가입 완료 후 /로 리다이렉트
+//   	return "redirect:/user/login";  // 회원가입 완료 후 로그인 페이지로 리다이렉트
    }
    
 //   /**
@@ -111,11 +111,6 @@ public class UserController {
 //		// true일 때 사용가능한 아이디(중복아이디X)		
 //		return !userService.existId(userId);	// !(아이디가 이미 존재하면 true, 없으면 false(사용 가능) 반환)
 //	}
-   
-   
-   
-   
-   
    
    /**
     * 로그인(공통) 화면 요청(security 하면 중복되는 내용)
--- tkForest/src/main/java/com/tkForest/service/UserService.java
+++ tkForest/src/main/java/com/tkForest/service/UserService.java
@@ -45,41 +45,79 @@ public class UserService {
         // 사용자가 입력한 비밀번호를 get => 암호화 encode => 다시 set 
         // sellerDTO.setPassword(bCryptPasswordEncoder.encode(sellerDTO.getPassword()));
         
-        sellerDTO.setSellerMemberNo("S2");
-        
+    	// sellerDTO.setSellerMemberNo("S2");
+    	sellerDTO.setSellerMemberNo(generateUniqueSellerMemberNo());
+    	
         // 존재하지 않는 ID일 경우 회원가입 처리
         SellerEntity sellerEntity = SellerEntity.toEntity(sellerDTO);
         sellerRepository.save(sellerEntity);	// 가입 성공
         return true;
  
     }
-
+    
     /**
+     * SellerMemberNo 중복되지 않도록 하기 위함(기존 최대값 +1)
+     * @return
+     */
+    private String generateUniqueSellerMemberNo() {
+    	String lastMemberNo = sellerRepository.findLastMemberNo(); // 최대 memberNo 조회
+        int newMemberNoSuffix;
+
+        if (lastMemberNo != null) {
+            newMemberNoSuffix = Integer.parseInt(lastMemberNo.substring(1)) + 1; // S 다음 숫자 증가
+        } else {
+            newMemberNoSuffix = 1; // 초기값 설정
+        }
+
+        return "S" + newMemberNoSuffix; // 새로운 SellerMemberNo 생성
+	}
+
+	/**
      * 전달 받은 buyerDTO를 buyerEntity로 변경한 후 DB에 저장.
      * @param buyerDTO
      * @return boolean
      */
     public boolean buyerSignUp(BuyerDTO buyerDTO) {
         
-    	// 가입하려는 ID가 이미 있으면 같은 ID로 가입 불가
-        boolean isExistId = sellerRepository.existsById(buyerDTO.getId()) 
-                            || buyerRepository.existsById(buyerDTO.getId());  // 셀러 또는 바이어 테이블에 이미 존재하면 true
-        
-        if (isExistId) {
-            // 이미 존재하는 ID라면 회원가입 불가 처리
-            return false;
-        }
+//    	// 가입하려는 ID가 이미 있으면 같은 ID로 가입 불가
+//      boolean isExistId = sellerRepository.findById(buyerDTO.getId()).isPresent() 
+//                          || buyerRepository.findById(buyerDTO.getId()).isPresent();  // 셀러 또는 바이어 테이블에 이미 존재하면 true
+//      
+//      if (isExistId) {
+//          // 이미 존재하는 ID라면 회원가입 불가 처리
+//          return false;
+//      }
 
-        // 비밀번호 암호화
-        // 사용자가 입력한 비밀번호를 get => 암호화 encode => 다시 set 
-        buyerDTO.setPassword(bCryptPasswordEncoder.encode(buyerDTO.getPassword()));
-        
-        // 존재하지 않는 ID일 경우 회원가입 처리
-        BuyerEntity buyerEntity = BuyerEntity.toEntity(buyerDTO);
-        buyerRepository.save(buyerEntity);	// 가입 성공
-        return true;
+      // 비밀번호 암호화
+      // 사용자가 입력한 비밀번호를 get => 암호화 encode => 다시 set 
+      // buyerDTO.setPassword(bCryptPasswordEncoder.encode(buyerDTO.getPassword()));
+      
+    	buyerDTO.setBuyerMemberNo("B1");
+//    	buyerDTO.setBuyerMemberNo(generateUniqueBuyerMemberNo());
+  	
+      // 존재하지 않는 ID일 경우 회원가입 처리
+      BuyerEntity buyerEntity = BuyerEntity.toEntity(buyerDTO);
+      buyerRepository.save(buyerEntity);	// 가입 성공
+      return true;
  
     }
+    
+    /**
+     * BuyerMemberNo 중복되지 않도록 하기 위함(기존 최대값 +1)
+     * @return
+     */
+    private String generateUniqueBuyerMemberNo() {
+    	String lastMemberNo = buyerRepository.findLastMemberNo(); // 최대 memberNo 조회
+        int newMemberNoSuffix;
+
+        if (lastMemberNo != null) {
+            newMemberNoSuffix = Integer.parseInt(lastMemberNo.substring(1)) + 1; // S 다음 숫자 증가
+        } else {
+            newMemberNoSuffix = 1; // 초기값 설정
+        }
+
+        return "B" + newMemberNoSuffix; // 새로운 BuyerMemberNo 생성
+	}
     
 //    /**
 //	 * (셀러) 이미 존재하는 ID인지 확인
