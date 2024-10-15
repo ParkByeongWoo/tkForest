5f35c9467f6923040c444ca6f30515faeeda0fa6
조현영 <sa99126@naver.com>
2024-10-15 11:26:40 +0900

    inquiryDetail과 답변 부분 html 수정

--- tkForest/src/test/java/com/example/tkForest/TkForestApplicationTests.java
+++ tkForest/src/test/java/com/example/tkForest/TkForestApplicationTests.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.tkForest.dto.SellerDTO;
+import com.tkForest.entity.InquiryEntity;
+import com.tkForest.repository.InquiryRepository;
+import com.tkForest.repository.ReplyRepository;
+import com.tkForest.service.InquiryService;
 import com.tkForest.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,9 +23,42 @@ class TkForestApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
 	void test_yeah () {
 		Object dto = userservice.existId("1", true);
 		log.info("회원: {}",dto.toString());
 	}
-}
+	
+	@Autowired
+	private ReplyRepository repository;	
+	
+//	// 게시글 여러 개를 삽입하는 테스트 코드
+//	@Test
+//	void testInsertBoard() {
+//		String[] w = {"이몽룡", "성춘향", "김방자", "전우치", "콩쥐", "팥쥐"};
+//		String[] c = {"나리나리 개나리 입에따다 물고요", 
+//				"송아지 송아지 얼룩 송아지 엄마소도 얼룩소", 
+//				"파란하늘 파란하늘 꿈이 드리운 푸른 언덕에..." ,
+//				"학교 종이 땡땡땡 어서모이자", 
+//				"동해물과 백두산이 마르고 닳도록 하느님이 보우하사", 
+//				"이 기상과 이 맘으로 충성을 다하여", 
+//				"가을 하늘 공활한데 높고 구름 없이", 
+//				"오늘은 목요일, 내일은 금요일이군요", 
+//				"날씨가 매우 좋네요.", 
+//				"아무 말이나 해 봅시다"};
+//		for(int i=0; i<10; ++i) {
+//			int index = (int)(Math.random() * w.length);
+//			String writer = w[index];
+//			
+//			index = (int)(Math.random() * c.length);
+//			String contents = c[index];
+//			String subject = "제목: " + contents;
+//			
+//			InquiryEntity entity = new InquiryEntity();
+//			entity.setInquiryWrite(buyerMemberNo);
+//			entity.setInquiryNo(subject);
+//			entity.setInquiryContent(contents);
+//			
+//			repository.save(entity);
+//		}
+	}
+//}
