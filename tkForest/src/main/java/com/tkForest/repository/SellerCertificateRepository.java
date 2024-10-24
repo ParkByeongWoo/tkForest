a262b7f647fd6893afb7b03ac31797134a3404c4
politecatyj <yjjo515a@gmail.com>
2024-10-24 14:24:42 +0900

    sellerCertificateDTO  포함 추적 되지 않던 파일  커밋

---
+++ tkForest/src/main/java/com/tkForest/repository/SellerCertificateRepository.java
@@ -0,0 +1,10 @@
+
+package com.tkForest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tkForest.entity.SellerCertificateEntity;
+
+public interface SellerCertificateRepository extends JpaRepository<SellerCertificateEntity, Integer> {
+
+}
